be16f2a290ae9053f2810a0ff58e28a3fcf2a666
VASATHE <vaishnavisathe9500@gmail.com>
2026-04-07 00:08:48 +0530

    Refactor BookController for code consistency

--- Book_Inventory_Management_System_using_React/backend/src/main/java/com/inventory/backend/controller/BookController.java
+++ Book_Inventory_Management_System_using_React/backend/src/main/java/com/inventory/backend/controller/BookController.java
@@ -1,58 +1,52 @@
 package com.inventory.backend.controller;
 
-import java.util.HashMap;
-import java.util.List;
-
+import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.Map;
-
+import org.springframework.web.bind.annotation.*;
 import com.inventory.backend.model.Book;
 import com.inventory.backend.service.BookService;
 
 import jakarta.validation.Valid;
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+
 @RestController
 @RequestMapping("/api/books")
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class BookController {
 
     @Autowired
     private BookService service;
 
     @GetMapping
-    public List<Book> getAllBooks(){
+    public List<Book> getAllBooks() {
         return service.getAllBooks();
     }
+
     @GetMapping("/dashboard")
     public Map<String, Long> getDashboard() {
-        Map<String, Long> data = new HashMap<>();
-        data.put("total", service.getTotalBooks());
-        data.put("lowStock", service.getLowStock());
-        data.put("outOfStock", service.getOutOfStock());
-        return data;
+        Map<String, Long> map = new HashMap<>();
+        map.put("total", service.getTotalBooks());
+        map.put("lowStock", service.getLowStock());
+        map.put("outOfStock", service.getOutOfStock());
+        return map;
     }
+
     @GetMapping("/{id}")
-    public Book getBook(@PathVariable int id){
+    public Book getBook(@PathVariable int id) {
         return service.getBookById(id);
     }
+
     @PostMapping
-    public Book addBook(@Valid @RequestBody Book book){
+    public Book addBook(@Valid @RequestBody Book book) {
         return service.saveBook(book);
     }
+
     @PutMapping("/{id}")
-    public Book updateBook(@PathVariable int id,@RequestBody Book book){
-        return service.updateBook(id,book);
+    public Book updateBook(@PathVariable int id, @RequestBody Book book) {
+        return service.updateBook(id, book);
     }
+
     @DeleteMapping("/{id}")
-    public void deleteBook(@PathVariable int id){
+    public void deleteBook(@PathVariable int id) {
         service.deleteBook(id);
     }
 }
